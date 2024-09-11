f079012e1fa9854cac538bf3d4fda22d0507b319
seonghyeoncho <chosh040@naver.com>
2024-09-11 21:38:21 +0900

    🚑 [hotfix] 민감 정보 환경 변수화

--- lotto/config/settings.py
+++ lotto/config/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!&@@b$5e2k9)was0)zjd7^gi!&yv(i!r2v2uy6ost-1*4oqjq3'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
